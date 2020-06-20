2fea5faf739ece14cd3475a01eacd0fa72f53266
Thiago Silva <thiago.silva@outlook.com>
2020-06-19 21:01:01 -0300

    Algumas funções com JavaScript

---
+++ JS/main.js
@@ -0,0 +1,54 @@
+
+// var d = new Date();
+// alert(d.getDay());
+// alert(d.getMonth() + 1);
+// alert(d.getHours());
+
+// var count;
+// for (count = 0; count <= 5; count++) {
+//     alert(count);
+// }
+
+// var count = 0;
+// while(count <= 5){
+//     console.log(count);
+//     alert(count);
+//     count++;
+// };
+
+// var idade = prompt("Qual sua idade?");
+// var idade = 18;
+// if(idade >= 18) {
+//     alert("maior de idade");
+// } else {
+//     alert("menor de idade");
+// };
+
+// var frutas = [{nome: "maçã", cor: "vermelha"} , {nome: "uva", cor: "roxa"}];
+// console.log(frutas);
+// alert(frutas[1].nome);
+
+// var fruta = {nome: "maçã", cor: "vermelha"};
+// console.log(fruta.nome);
+// alert(fruta.cor);
+
+// var lista = ["maçã", "pêra", "laranja"];
+// lista.push("uva");
+// lista.pop();
+
+// console.log(lista);
+// console.log(lista.reverse());
+// console.log(lista.toString());
+// console.log(lista.join(" - "));
+
+// var nome = "Thiago da Silva";
+// var n1 = 29;
+// var n2 = 10;
+// var frase = "Japão é o melhor time do mundo";
+// alert(nome + " tem " + n1 + " anos");
+// alert(n1 + n2);
+// console.log(nome);
+// console.log(n1 + n2);
+// console.log(frase.toUpperCase());
+// console.log(frase.toLowerCase());
+// alert(frase.replace("Japão", "Brasil"));
